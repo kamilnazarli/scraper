8b73556c01f661e4a3602532670d4416a83d3714
Kamil <nezerovk425@gmail.com>
2026-09-04 12:33:49 +0400

    Stage 1: fetch and cache HTML

--- src/main.py
+++ src/main.py
@@ -1,5 +1,23 @@
 import requests
+import os
+from pathlib import Path
 
-r = requests.get("https://books.toscrape.com/robots.txt")
+headers = {
+    "user-agent": "https://github.com/kamilnazarli/scraper"
+}
+    
+path = Path(f"cache/catalogue-page-1.html")
+path.parent.mkdir(parents=True, exist_ok=True)
 
-print(r)
+if not path.exists():
+    r = requests.get(f"https://books.toscrape.com/catalogue/page-1.html",
+                     headers=headers,
+                     timeout=3)
+
+    if r.status_code == requests.codes.ok:
+        path.write_text(r.text, encoding="utf-8")
+    else:
+        r.raise_for_status()
+else:
+    print("This path already exists")
+
